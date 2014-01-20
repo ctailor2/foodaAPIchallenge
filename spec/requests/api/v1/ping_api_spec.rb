72bea6ac2944e648f9061e4a7361e5454d3d608a
Chirag Tailor <ctailor2@gmail.com>
2014-01-20 00:02:32 -0600

    Ping API request spec

---
+++ spec/requests/api/v1/ping_api_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Ping API" do
+	before { create(:application) }
+
+	context "with a valid API key" do
+		before { get('/api/v1/', nil, { 'Authorization' => 'Token token="secret"' }) }
+
+		it "returns 200 OK" do
+			expect(response.status).to eq(200)
+		end
+
+		it "responds with a welcome message" do
+			json = JSON.parse(response.body, symbolize_names: true)
+			expect(json[:message]).to eq("Welcome to the Fooda API!")
+		end
+	end
+
+	context "with an invalid API key" do
+		before { get('/api/v1/', nil, { 'Authorization' => 'Token token="bananas"' }) }
+
+		it "returns 401 unauthorized" do			
+			expect(response.status).to eq(401)
+		end
+	end
+end
